533bdbbe0847ce3f307c8326a48dd9514662b5fa
Vachek-hooves <vachek0602@gmail.com>
2025-01-24 18:41:44 +0200

    add images

--- data/RoyalData.js
+++ data/RoyalData.js
@@ -6,7 +6,7 @@ export const RoyalData = [
     id: 1,
     name: 'Henry VIII',
     year: '1491-1547',
-    // imagePath:'../assets/royal/',
+    imagePath: '../assets/image/royal/henry.png',
     about:
       'Henry VIII (1491–1547) is remembered as the king who reshaped the religious and political landscape of England. Famous for his six marriages, his reign was defined by his desperate quest for a male heir. When the Pope refused to annul his first marriage to Catherine of Aragon, Henry broke with the Catholic Church, creating the Church of England and declaring himself its head. This act initiated the English Reformation. His marriage to Anne Boleyn, who failed to produce a male heir, ended with her execution on charges of adultery and treason—allegations widely believed to have been fabricated. Despite his personal controversies, Henry VIII modernized the English navy and centralized royal authority, leaving a complex legacy as both a tyrant and a reformer.',
   },
@@ -17,7 +17,7 @@ export const RoyalData = [
     region: 'England',
     name: ' Elizabeth I',
     year: '1558-1603',
-    // imagePath:'../assets/royal/elizabeth.png',
+    imagePath: '../assets/image/royal/elizabeth.png',
     about:
       'Henry VIII s daughter by Anne Boleyn, ascended to the throne after years of political turmoil and ruled for 45 years during the Elizabethan Era. Known as the “Virgin Queen” for her decision to remain unmarried, she skillfully navigated the treacherous waters of diplomacy, balancing Protestant and Catholic factions in England. Her reign saw the defeat of the Spanish Armada in 1588, a pivotal moment that established England as a naval power. Under her leadership, England flourished culturally, with luminaries like William Shakespeare and Christopher Marlowe shaping the English Renaissance. Her careful diplomacy and strong leadership ensured a relatively stable and prosperous reign, solidifying her as one of England’s greatest monarchs.',
   },
@@ -26,9 +26,9 @@ export const RoyalData = [
     era: 'Renaissance',
     id: 3,
     region: 'France',
-    name: ' Louis XIV',
+    name: 'Louis XIV',
     year: '1643-1715',
-    // imagePath:'../assets/royal/charles.png',
+    imagePath: '../assets/image/royal/louis.png',
     about:
       'the “Sun King,” ruled for an astonishing 72 years, making him one of the longest-reigning monarchs in history. He epitomized absolute monarchy, famously declaring, “L’état, c’est moi” (“I am the state”). Louis transformed France into a dominant European power through his centralized government and lavish spending. The Palace of Versailles, a symbol of his grandeur, became the cultural and political hub of Europe. Despite his success in strengthening France’s influence, his constant wars and extravagant lifestyle drained the country’s finances, sowing seeds of discontent that would contribute to the French Revolution.',
   },
@@ -39,7 +39,7 @@ export const RoyalData = [
     region: 'Russia',
     name: 'Catherine II',
     year: '1729-1796',
-    // imagePath:'../assets/royal/catherine.png',
+    imagePath: '../assets/image/royal/catherine.png',
     about:
       'was a German princess who became one of Russia’s most celebrated rulers after overthrowing her husband, Peter III. Her reign marked a golden age for the Russian Empire, characterized by territorial expansion, cultural development, and modernization. A patron of the arts and Enlightenment thought, she corresponded with leading philosophers like Voltaire and Diderot. Catherine’s reforms aimed to modernize Russia’s government and economy, though her policies often favored the nobility, leaving the peasantry burdened. Her efforts to secularize church lands and promote education left a lasting impact, transforming Russia into a major European power.',
   },
@@ -50,7 +50,7 @@ export const RoyalData = [
     region: 'France',
     name: 'Charlemagne',
     year: '742–814',
-    // imagePath:'../assets/royal/peter.png',
+    imagePath: '../assets/image/royal/charlemagne.png',
     about:
       'was a medieval king who united much of Western Europe and became the first Holy Roman Emperor in 800. Known as the “Father of Europe,” Charlemagne’s reign marked the Carolingian Renaissance, a revival of art, culture, and education. He established a strong centralized government and standardized laws, helping to lay the foundation for modern European states. A devout Christian, he sought to spread Christianity through conquest, converting conquered peoples across his vast empire. His legacy endures in his contributions to European unity and cultural advancement, as well as in the title of Holy Roman Emperor, which symbolized the connection between church and state for centuries.',
   },
@@ -61,7 +61,7 @@ export const RoyalData = [
     region: 'Spain',
     year: '1556-1621',
     name: 'Philip II of Spain',
-    // imagePath:'../assets/royal/charlemagne.png',
+    imagePath: '../assets/image/royal/philip.png',
     about:
       'wwas the king of Spain, Portugal, Naples, and Sicily, as well as the ruler of a vast colonial empire in the Americas and Asia. His reign was marked by efforts to strengthen Catholicism and expand Spanish influence in Europe. Philip built the Escorial, a palace-monastery symbolizing his power and faith. One of the most dramatic moments of his rule was the defeat of the Spanish Armada in 1588, sent to invade England. Despite this loss, his reign laid the foundation for Spain’s “Golden Age,” when the country reached cultural and artistic heights.',
   },
@@ -72,7 +72,7 @@ export const RoyalData = [
     region: 'France',
     year: '1769-1821',
     name: 'Napoleon Bonaparte',
-    // imagePath:'../assets/royal/napoleon.png',
+    imagePath: '../assets/image/royal/napoleon.png',
     about:
       'was a French military leader who rose to prominence during the French Revolution and became Emperor of France. Known as the “Little Corporal,” he led France to numerous victories in Europe, but his aggressive expansionist policies led to his downfall. Napoleon’s rule was marked by his military campaigns and his strained relationship with the nobility. His legacy is complex, as he was both a great military leader and a ruthless autocrat. His legacy endures in his contributions to French history and his legacy as a military leader.',
   },
@@ -83,7 +83,7 @@ export const RoyalData = [
     region: 'Russia',
     year: '1530-1584',
     name: 'Ivan the Terrible',
-    // imagePath:'../assets/royal/ivan.png',
+    imagePath: '../assets/image/royal/ivan.png',
     about:
       'was a Russian tsar who ruled from 1533 to 1584. Known as the “Terrible,” he was a ruthless and paranoid leader who expanded the Russian Empire and modernized the country. His rule was marked by his paranoia and his violent suppression of the nobility. His legacy is complex, as he was both a great leader and a ruthless autocrat. His legacy endures in his contributions to Russian history and his legacy as a military leader.',
   },
@@ -94,7 +94,7 @@ export const RoyalData = [
     region: 'Russia',
     year: '1672-1725',
     name: 'Peter the Great',
-    // imagePath:'../assets/royal/peter.png',
+    imagePath: '../assets/image/royal/peter.png',
     about:
       'was a Russian tsar who transformed his country into a major European power. Born to a weak and divided Russia, he seized power at a young age and embarked on a series of reforms that modernized Russia. He modernized the Russian navy, expanded the empire, and established St. Petersburg as the new capital. His reforms included the abolition of serfdom and the introduction of Western technology and education. His rule was marked by his military campaigns and his strained relationship with the nobility. His legacy is complex, as he was both a great reformer and a ruthless autocrat.',
   },
@@ -105,7 +105,7 @@ export const RoyalData = [
     region: 'England',
     year: '1819-1901',
     name: 'Queen Victoria',
-    // imagePath:'../assets/royal/victoria.png',
+    imagePath: '../assets/image/royal/victoria.png',
     about:
       'was the longest-reigning British monarch, reigning for 63 years. Known as the “Grandmother of Europe,” she presided over a period of unprecedented global expansion and industrialization. Her reign saw the expansion of the British Empire, with colonies in Africa, Asia, and the Americas. She was a staunch defender of the British monarchy and a symbol of stability and continuity. Her legacy endures in her role as the “Grandmother of Europe,” as well as in her contributions to British history and her legacy as a monarch.',
   },
@@ -116,8 +116,10 @@ export const RoyalData = [
     region: 'France',
     name: 'Clovis I',
     year: 'c. 466–511',
+    imagePath: '../assets/image/royal/clovis.png',
     about:
       'was the first king of the Franks, a powerful Germanic tribe that eventually became the rulers of Gaul. He converted to Christianity in 496, which helped to unite his people and laid the foundation for the development of the Frankish kingdom. Clovis’s conversion to Christianity was a pivotal moment in the history of France, as it helped to establish the Catholic Church as the dominant religion in the country. His legacy endures in his role as the first king of the Franks and his contributions to the development of the Frankish kingdom.',
+
   },
   {
     dynasty: 'Habsburgs',
@@ -126,6 +128,7 @@ export const RoyalData = [
     region: 'German',
     name: 'Frederick II',
     year: '1194–1250',
+    imagePath: '../assets/image/royal/frederick.png',
     about:
       ' known as “Frederick the Great” and “Stupor Mundi” (“Wonder of the World”), was Holy Roman Emperor and King of Sicily. Renowned for his intellect and cultural achievements, he was a patron of science, literature, and the arts. Frederick enacted legal reforms, promoted religious tolerance, and engaged in diplomatic negotiations during the Crusades, including a peaceful takeover of Jerusalem. His reign blended Eastern and Western influences, making his court in Palermo a vibrant cultural hub. Despite conflicts with the papacy, he is celebrated as one of the most remarkable rulers of the Middle Ages',
   },
@@ -136,6 +139,7 @@ export const RoyalData = [
     region: 'Austria',
     name: 'Maria Theresa',
     year: '1717-1780',
+    imagePath: '../assets/image/royal/maria.png',
     about:
       'was the only female ruler of the Habsburg dynasty and one of the most influential monarchs of 18th-century Europe. She modernized the Austrian economy, centralized administration, and reformed education and the military. Maria Theresa’s pragmatic diplomacy, particularly through her strategic marital alliances, earned her the title “Mother-in-law of Europe.” She successfully defended her territories during the War of the Austrian Succession and initiated reforms that strengthened the Habsburg Monarchy, leaving a lasting legacy of modernization.',
   },
@@ -146,6 +150,7 @@ export const RoyalData = [
     region: 'Ancient Macedonia',
     name: 'Alexander the Great',
     year: '356-323 BCE',
+    imagePath: '../assets/image/royal/alexander.png',
     about:
       'was a king of Macedon who created one of the largest empires in history by the age of 30, stretching from Greece to Egypt and into India. A brilliant military strategist, he never lost a battle, conquering the Persian Empire and spreading Hellenistic culture across his territories. Alexander’s legacy includes the founding of cities such as Alexandria in Egypt, which became centers of learning and culture. His sudden death at 32 left his empire fragmented but profoundly influenced the ancient world, shaping the course of Western and Eastern civilizations.',
   },
@@ -156,6 +161,7 @@ export const RoyalData = [
     region: 'England',
     name: 'Alfred the Great',
     year: '849-899',
+    imagePath: '../assets/image/royal/alfred.png',
     about:
       'was King of Wessex and is remembered for his defense of England against Viking invasions. A visionary leader, he reorganized the military, established a navy, and constructed fortified towns known as burhs. Alfred was also a patron of education and culture, commissioning translations of key texts into Old English and promoting literacy. His reign laid the foundation for the unification of England, earning him the title “the Great” for his military and cultural achievements.',
   },
@@ -166,6 +172,7 @@ export const RoyalData = [
     region: 'Ottoman Empire',
     name: 'Suleiman the Magnificent',
     year: '1494-1566',
+    imagePath: '../assets/image/royal/suleiman.png',
     about:
       'was the 10th and longest-reigning Sultan of the Ottoman Empire. Under his rule, the empire reached its territorial zenith, stretching across three continents. Suleiman was not only a brilliant military commander but also a renowned lawmaker, earning the title “The Lawgiver” in the Islamic world. His reign saw a flourishing of the arts, architecture, and literature, exemplified by the construction of the Suleymaniye Mosque in Istanbul. Suleiman’s leadership strengthened the Ottoman Empire, making it a dominant power in the 16th century and leaving a legacy of cultural and legal advancements.',
   },
